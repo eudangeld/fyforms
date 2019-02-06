1bd25f5c7bf08ed78e13111a8fdb48ce8641ad8d
Dan <dan@dopaminamob.com.br>
2019-02-06 16:55:56 -0200

    adding theme options

--- lib/src/widgets/fy_form_field.dart
+++ lib/src/widgets/fy_form_field.dart
@@ -8,6 +8,7 @@ class FyFormField extends StatelessWidget {
     Key key,
   }) : super(key: key);
   dynamic fieldData;
+  BuildContext context;
 
   @override
   Widget build(BuildContext context) {
@@ -15,20 +16,29 @@ class FyFormField extends StatelessWidget {
   }
 
   Widget _buildTextField() {
-    return Expanded(
-      flex: fieldData['column']['flex'],
-      child: TextFormField(
-        validator: fieldData['column']['validator'] != null
-            ? fyvalidators.validators[fieldData['column']['validator']]
-            : (value) => value,
-        decoration: InputDecoration(
-            icon: Icon(fycons.t[fieldData['column']['icon']]),
-            hintText: fieldData['column']['hint'],
-            labelText: fieldData['column']['label'],
-            enabledBorder: const OutlineInputBorder(
-              borderSide: BorderSide(style: BorderStyle.solid),
-            )),
-      ),
+    return Builder(
+      builder: (BuildContext context) {
+        return Expanded(
+          flex: fieldData['column']['flex'],
+          child: TextFormField(
+            validator: fieldData['column']['validator'] != null
+                ? fyvalidators.validators[fieldData['column']['validator']]
+                : (value) => value,
+            decoration: InputDecoration(
+                hintStyle: Theme.of(context).textTheme.display1,
+                labelStyle: Theme.of(context).textTheme.title,
+                icon: Icon(
+                  fycons.t[fieldData['column']['icon']],
+                  color: Theme.of(context).textTheme.title.color,
+                ),
+                hintText: fieldData['column']['hint'],
+                labelText: fieldData['column']['label'],
+                enabledBorder: const OutlineInputBorder(
+                  borderSide: BorderSide(style: BorderStyle.solid),
+                )),
+          ),
+        );
+      },
     );
   }
 }
